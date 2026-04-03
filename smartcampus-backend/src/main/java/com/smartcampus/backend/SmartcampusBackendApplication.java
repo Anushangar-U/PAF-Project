437745aba6fe9f6800c2b27108b964c7936199ae
Anushangar-U <u.anushangar@gmail.com>
2026-04-03 17:28:52 +0530

    Initial Spring Boot and README for facilities catalogue

---
+++ smartcampus-backend/src/main/java/com/smartcampus/backend/SmartcampusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartcampus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcampusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcampusBackendApplication.class, args);
+	}
+
+}

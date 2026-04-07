3987809c841dc98e3be23c2888aa0b6dd70d06de
NajlaMNF <najlanaleem9@gmail.com>
2026-04-07 15:51:50 +0530

    Add BookingForm component for resource booking functionality

--- smartcampus-frontend/src/App.js
+++ smartcampus-frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+
 import ResourceList from './components/ResourceList';
+import BookingForm from './components/BookingForm';
 
 function App() {
   return (
     <div className="App">
+      <h2>Book a Resource</h2>
+      <BookingForm />
+      <hr style={{margin: '2rem 0'}} />
       <ResourceList />
     </div>
   );

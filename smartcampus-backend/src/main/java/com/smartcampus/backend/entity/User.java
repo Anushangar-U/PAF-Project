2e83deac96f9114d3fa6f4b0cbfc9ba70bd1a161
Gowsig-2000 <iit23808652@my.sliit.lk>
2026-04-18 12:26:10 +0530

    add User and Role entities with UserRepository interface for authentication

---
+++ smartcampus-backend/src/main/java/com/smartcampus/backend/entity/User.java
@@ -0,0 +1,77 @@
+package com.smartcampus.backend.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String email;
+    private Role role;
+
+    private String provider;     // GOOGLE or LOCAL
+    private String providerId;   // Google unique ID
+
+    public User() {
+    }
+
+    public User(String name, String email, Role role, String provider, String providerId) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+}
